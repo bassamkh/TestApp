93e1c519e317fcc0aac0af40ee5a8782c751e876
BKhoury <bassam-el-khoury@live.com>
2019-03-30 15:37:02 +0200

    Revert "Making test SpringBoot Application (#1)" (#3)
    This reverts commit b09f5f0f688ec1c264ad2be41b8e49669773efa5.

--- src/main/java/com/SpringBoot/TestApp/model/Affiliate.java
+++
@@ -1,7 +0,0 @@
-package com.SpringBoot.TestApp.model;
-
-public class Affiliate extends User {
-	public Affiliate() {
-		setPercentage(0.1);
-	}
-}
--- src/main/java/com/SpringBoot/TestApp/model/Employee.java
+++
@@ -1,7 +0,0 @@
-package com.SpringBoot.TestApp.model;
-
-public class Employee extends User {
-	public Employee() {
-		setPercentage(0.3);
-	}
-}
